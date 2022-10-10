06e8f308cbab0aba8c43fc8a1c37de09ec8564d6
hyhwaxung <hyhwaxung@gmail.com>
2022-10-11 00:29:57 +0900

    2022.10.11 설정 화면 UI 구현

--- weatherApp/CityListViewController.swift
+++ weatherApp/CityListViewController.swift
@@ -30,3 +30,4 @@ extension CityListViewController: UISearchBarDelegate {
         
     }
 }
+
---
+++ weatherApp/SettingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingViewController.swift
+//  weatherApp
+//
+//  Created by 정현화 on 2022/10/10.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
